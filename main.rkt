9ee7d4399ddaa484d1645bf762bc971ec4779f68
MontyMango <cameronharter50@hotmail.com>
2024-03-05 11:53:41 -0500

    Archived website page & HTML rendering on main.rkt

--- main.rkt
+++ main.rkt
@@ -1,26 +1,56 @@
-; Group 3: Cameron Harter, 
+; Group 3: Cameron Harter, (add your names here!)
 ; Click run and the web server will start automatically with the website popping up automatically
-
-#lang web-server/insta
-(define (start request)
-  (response/xexpr
-   '(html
-     ; HTML Head
-     (head
-      (title "Group 4's Blog")
-      )
-
-     ; HTML Body
-     (body
-      (h1 "This website is under construction!")
-     )
-    )
-  )
-)
-
-; Below here is some syntax that will be used in this program
 ; Reference: https://docs.racket-lang.org/continue/index.html
+#lang web-server/insta
+
+; The body of a "post"
+(struct post (title body))
+
+(define posts
+  (post "Post 2"
+   ))
 
 
-; #3: Basic Blog
-; (define BLOG (list (post "First Post!" "Hey, this is my first post!")))
+
+
+
+; |==============================|
+; | HTML RENDERING               |
+; |==============================|
+; There are multiple ways to render the HTML (Follow section 4 for different ways!)
+; Uncomment the function you want to use!
+
+; ==========================
+; | X-expression - Function|
+; ==========================
+; DESCRIPTION: This is the "simplest" way of rendering the HTML,
+; by converting quotation marks (" ") into body text but the
+; drawback is you can only do body text with this rendering.
+
+;(define xexpr/c
+;  (flat-rec-contract
+;   xexpr
+;   (or/c string?
+;         (cons/c symbol? (listof xexpr))
+;         (cons/c symbol?
+;                 (cons/c (listof (list/c symbol? string?))
+;                         (listof xexpr))))))
+; ==============================================================
+
+; =================
+; | X-expression |
+; =================
+; DESCRIPTION: This is the "simplest" way of rendering the HTML,
+; by converting quotation marks (" ") into body text but the
+; drawback is you can only do body text with this rendering.
+
+;(define xexpr/c
+;  (flat-rec-contract
+;   xexpr
+;   (or/c string?
+;         (cons/c symbol? (listof xexpr))
+;         (cons/c symbol?
+;                 (cons/c (listof (list/c symbol? string?))
+;                         (listof xexpr))))))
+; ==============================================================
+
